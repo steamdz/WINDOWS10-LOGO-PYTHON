56c2d52dcd1b384fed0e236a025620ae8d1abb78
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2022-03-05 23:16:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#AYMEN DEV
+#WINDOWS 10
+
+from turtle import *
+ 
+speed(1)
+bgcolor("white")
+penup()
+goto(-50, 60)
+pendown()
+color('#00adef')
+begin_fill()
+goto(100, 100)
+goto(100, -100)
+goto(-50, -60)
+goto(-50, 60)
+end_fill()
+color("white")
+goto(15, 100)
+color("white")
+width(10)
+goto(15, -100)
+penup()
+goto(100, 0)
+pendown()
+goto(-100, 0)
+done()
